940b3e99a20daa60547a2ff15076fe6c8a200bc0
Huynh Quang Thao <huynhquangthao@gmail.com>
2013-10-28 13:18:45 +0700

    first initialize project

---
+++ Hop Am Chuan/Hop Am Chuan/src/main/java/com/hqt/hac/view/MainActivity.java
@@ -0,0 +1,193 @@
+package com.hqt.hac.view;
+
+import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
+import android.os.Build;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.widget.DrawerLayout;
+import android.widget.SearchView;
+import android.widget.TextView;
+
+import com.hqt.hac.fragment.NavigationDrawerFragment;
+
+public class MainActivity extends ActionBarActivity
+        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+
+    /**
+     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
+     */
+    private NavigationDrawerFragment mNavigationDrawerFragment;
+
+    /**
+     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
+     */
+    private CharSequence mTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mNavigationDrawerFragment = (NavigationDrawerFragment)
+                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
+        mTitle = getTitle();
+
+        // Set up the drawer.
+        mNavigationDrawerFragment.setUp(
+                R.id.navigation_drawer,
+                (DrawerLayout) findViewById(R.id.drawer_layout));
+    }
+
+    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+        // update the main content by replacing fragments
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
+                .commit();
+    }
+
+    public void onSectionAttached(int number) {
+        switch (number) {
+            case 1:
+                mTitle = getString(R.string.title_section1);
+                break;
+            case 2:
+                mTitle = getString(R.string.title_section2);
+                break;
+            case 3:
+                mTitle = getString(R.string.title_section3);
+                break;
+        }
+    }
+
+    public void restoreActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setTitle(mTitle);
+    }
+
+
+    /**
+     * Create ActionBar items
+     * Items of Activity will be created before
+     * Items of Fragment will be created after that
+     * always try to call super method
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // if Drawer is open. Call Default Method
+        // Let the Drawer decide what to show in the action bar
+        if (mNavigationDrawerFragment.isDrawerOpen()) {
+            return super.onCreateOptionsMenu(menu);
+        }
+
+        // Only show items in the action bar relevant to this screen
+        // if the drawer is not showing.
+        // getMenuInflater().inflate(R.menu.main, menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+
+        // because SearchView just exist in HoneyComb 3.0 to above
+        // we should check version of users here
+        // if version is lower. We use SearchDialog instead
+        // TODO: search google. Find SearchWidget library for API Lower than 11
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+
+            // Get the SearchView and set the Search Configuration
+            SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+            SearchView searchView = (SearchView) menu.findItem(R.id.search_bar).getActionView();
+
+            // Assumes current activity is the searchable activity
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+            // Do not icon the widget. expand it.
+            searchView.setIconifiedByDefault(false);
+
+        }
+
+        restoreActionBar();
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * when search dialog is active. activity will lost input focus
+     * so. do some stuffs (such as saving state) before search
+     * Unless passing search context data.
+     * should end method by calling super class implementation
+     */
+    @Override
+    public boolean onSearchRequested() {
+        // doing some stuff before here
+        return super.onSearchRequested();
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            textView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }
+
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+            ((MainActivity) activity).onSectionAttached(
+                    getArguments().getInt(ARG_SECTION_NUMBER));
+        }
+    }
+
+}
---
+++ Hop Am Chuan/Hop Am Chuan/src/main/java/com/hqt/hac/view/SearchViewActivity.java
@@ -0,0 +1,111 @@
+package com.hqt.hac.view;
+
+import android.app.SearchManager;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Search Activity acts as Search Dialog
+ * System will call when receive Search Intent
+ * See this link for more Detail:
+ *
+ * @link{http://developer.android.com/guide/topics/search/search-dialog.html#UsingSearchWidget}
+ */
+public class SearchViewActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_view);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+
+        handleIntent(getIntent());
+
+
+    }
+
+    /**
+     * If the currently Activity is searchable Activity, then one of two things will be happens :
+     *
+     * 1) <b> By default: </b>
+     * the searchable activity receives the ACTION_SEARCH intent with a call to onCreate()
+     * and a new instance of the activity is brought to the top of the activity stack.
+     * There are now two instances of your searchable activity in the activity stack
+     * (so pressing the Back button goes back to the previous instance of the searchable activity,
+     * rather than exiting the searchable activity).
+     *
+     * 2) <b> If you set android:launchMode to "singleTop" (Set in Android Manifest): </b>
+     * then the searchable activity receives the ACTION_SEARCH intent with a call to onNewIntent(Intent),
+     * passing the new ACTION_SEARCH intent here.
+     * <hqt>
+     * This action usually ideal.
+     * Because chances are goods that once a search is done, the user will performs additional searches
+     * bad practice if app creates multiple instances of the searchable activity !!!
+     * </hqt>
+     */
+    @Override
+    protected void onNewIntent(Intent intent) {
+        setIntent(intent);
+        handleIntent(intent);
+    }
+
+    /**
+     * private helper method. So both way will do the same for Search Activity
+     */
+    private void handleIntent(Intent intent) {
+        // Verify the action and get the query
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            // perform search base on query here
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.search_view, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_search_view, container, false);
+            return rootView;
+        }
+    }
+
+}
