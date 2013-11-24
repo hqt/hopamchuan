8d2302f2a586ec0576d8fbc1880941d0dbb87ad8
Huynh Quang Thao <huynhquangthao@gmail.com>
2013-11-24 11:10:04 +0700

    add delete row in listview animation

--- Hop Am Chuan/Hop Am Chuan/src/main/java/com/hqt/hac/helper/widget/ListViewWidget.java
+++ Hop Am Chuan/Hop Am Chuan/src/main/java/com/hqt/hac/helper/widget/ListViewWidget.java
@@ -1,22 +1,44 @@
 package com.hqt.hac.helper.widget;
 
-import android.app.Activity;
 import android.content.Context;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewConfiguration;
+import android.view.ViewTreeObserver;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
+
+import com.hqt.hac.helper.adapter.IArrayAdapter;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * animation on ListView
  */
 public class ListViewWidget {
 
+    /** Constant variables for animation */
+    public static final int SWIPE_DURATION = 250;
+    public static final int MOVE_DURATION = 150;
+
     /**
      * Handle touch events to fade/move dragged items as they are swiped out
+     * TODO : NEED TO EXTENDS TO BASE ADAPTER RATHER THAN ARRAY ADAPTER (THAOHQ)
      */
-    public View.OnTouchListener getTouchListener(final Context context, final ListView listView) {
-        View.OnTouchListener mTouchListener = new View.OnTouchListener() {
+    public static View.OnTouchListener getTouchListener(final Context context, final ListView listView, final ArrayAdapter adapter, final BackgroundContainer mBackgroundContainer) {
+
+        /**
+         * Use HashMap with Identity for supporting remove action
+         * declare outside for performance : prevent initialize to many times
+         * map from itemId to y-location of item (by using View.getTop())
+         */
+        final Map<Long, Integer> mItemIdTopMap = new HashMap<Long, Integer>();
+
+
+        View.OnTouchListener mTouchListener;
+        mTouchListener = new View.OnTouchListener() {
 
             float mDownX;
             private int mSwipeSlop = -1;
@@ -93,7 +115,7 @@ public class ListViewWidget {
                             // velocity (via the VelocityTracker class) to send the item off or
                             // back at an appropriate speed.
                             long duration = (int) ((1 - fractionCovered) * SWIPE_DURATION);
-                            mListView.setEnabled(false);
+                            listView.setEnabled(false);
                             v.animate().setDuration(duration).
                                     alpha(endAlpha).translationX(endX).
                                     withEndAction(new Runnable() {
@@ -103,11 +125,11 @@ public class ListViewWidget {
                                             v.setAlpha(1);
                                             v.setTranslationX(0);
                                             if (remove) {
-                                                animateRemoval(mListView, v);
+                                                animateRemoval(v);
                                             } else {
                                                 mBackgroundContainer.hideBackground();
                                                 mSwiping = false;
-                                                mListView.setEnabled(true);
+                                                listView.setEnabled(true);
                                             }
                                         }
                                     });
@@ -120,87 +142,117 @@ public class ListViewWidget {
                 }
                 return true;
             }
+
+
+            /**
+             * This method animates all other views in the ListView container (not including ignoreView)
+             * into their final positions. It is called after ignoreView has been removed from the
+             * adapter, but before layout has been run. The approach here is to figure out where
+             * everything is now, then allow layout to run, then figure out where everything is after
+             * layout, and then to run animations between all of those start/end positions.
+             *
+             * Helper function bases on view object rather than position for general purpose
+             */
+            void animateRemoval(View viewToRemove) {
+
+                int firstVisiblePosition = listView.getFirstVisiblePosition();
+                // save all y-location of visible items on ListView (except remove item)
+                for (int i = 0; i < listView.getChildCount(); ++i) {
+                    View child = listView.getChildAt(i);
+                    if (child != viewToRemove) {
+                        int position = firstVisiblePosition + i;
+                        long itemId = adapter.getItemId(position);
+                        mItemIdTopMap.put(itemId, child.getTop());
+                    }
+                }
+
+                // Delete the item from the adapter
+                int position = listView.getPositionForView(viewToRemove);
+                adapter.remove(adapter.getItem(position));
+
+                /**
+                 * TODO : DOCUMENT CLEARER HERE FOR REUSABLE (THAOHQ)
+                 */
+                final ViewTreeObserver observer = listView.getViewTreeObserver();
+                if (observer == null) return;
+
+                observer.addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+                    public boolean onPreDraw() {
+                        observer.removeOnPreDrawListener(this);
+                        // first animation : mark animation for delete row before animate other rows
+                        boolean firstAnimation = true;
+                        int firstVisiblePosition = listView.getFirstVisiblePosition();
+
+                        // listView.getCount() : return of all items in Adapter
+                        // listView.getChildCount() : return number of subviews (often, is number of rows in screen and approximate ~= 10)
+                        // important note : position of them will never relate to the actual item position in the adapter.
+                        // so we must convert this position here to item position by using listView.getFirstVisiblePosition()
+                        for (int i = 0; i < listView.getChildCount(); ++i) {
+                            final View child = listView.getChildAt(i);
+                            // real id of item on screen
+                            int position = firstVisiblePosition + i;
+                            long itemId = adapter.getItemId(position);
+                            Integer startTop = mItemIdTopMap.get(itemId);
+                            int top = child.getTop();
+                            if (startTop != null) {
+                                if (startTop != top) {
+                                    int delta = startTop - top;
+                                    child.setTranslationY(delta);
+                                    child.animate().setDuration(MOVE_DURATION).translationY(0);
+                                    // animated removed row
+                                    if (firstAnimation) {
+                                        child.animate().withEndAction(new Runnable() {
+                                            public void run() {
+                                                mBackgroundContainer.hideBackground();
+                                                mSwiping = false;
+                                                listView.setEnabled(true);
+                                            }
+                                        });
+                                        firstAnimation = false;
+                                    }
+                                }
+                            } else {
+                                // this new view does not show anytime before
+                                // so maybe at the bottom or the top
+                                //
+                                // Animate new views along with the others. The catch is that they did not
+                                // exist in the start state, so we must calculate their starting position
+                                // based on neighboring views.
+                                int childHeight = child.getHeight() + listView.getDividerHeight();
+                                startTop = top + (i > 0 ? childHeight : -childHeight);
+                                int delta = startTop - top;
+                                child.setTranslationY(delta);
+                                child.animate().setDuration(MOVE_DURATION).translationY(0);
+                                // animated removed row
+                                if (firstAnimation) {
+                                    child.animate().withEndAction(new Runnable() {
+                                        public void run() {
+                                            mBackgroundContainer.hideBackground();
+                                            mSwiping = false;
+                                            listView.setEnabled(true);
+                                        }
+                                    });
+                                    firstAnimation = false;
+                                }
+                            }
+                        }
+
+                        // finish animation. clear all data in map
+                        mItemIdTopMap.clear();
+                        return true;
+                    }
+                });
+            }
         };
 
-        return mOnTouchListener;
-
+        return mTouchListener;
     }
 
     /**
-     * This method animates all other views in the ListView container (not including ignoreView)
-     * into their final positions. It is called after ignoreView has been removed from the
-     * adapter, but before layout has been run. The approach here is to figure out where
-     * everything is now, then allow layout to run, then figure out where everything is after
-     * layout, and then to run animations between all of those start/end positions.
+     * Helper method for retrieve item id of i-th item of visible rows in ListView
      */
-    private void animateRemoval(final ListView listview, View viewToRemove) {
-        int firstVisiblePosition = listview.getFirstVisiblePosition();
-        for (int i = 0; i < listview.getChildCount(); ++i) {
-            View child = listview.getChildAt(i);
-            if (child != viewToRemove) {
-                int position = firstVisiblePosition + i;
-                long itemId = mAdapter.getItemId(position);
-                mItemIdTopMap.put(itemId, child.getTop());
-            }
-        }
-        // Delete the item from the adapter
-        int position = mListView.getPositionForView(viewToRemove);
-        mAdapter.remove(mAdapter.getItem(position));
-
-        final ViewTreeObserver observer = listview.getViewTreeObserver();
-        observer.addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
-            public boolean onPreDraw() {
-                observer.removeOnPreDrawListener(this);
-                boolean firstAnimation = true;
-                int firstVisiblePosition = listview.getFirstVisiblePosition();
-                for (int i = 0; i < listview.getChildCount(); ++i) {
-                    final View child = listview.getChildAt(i);
-                    int position = firstVisiblePosition + i;
-                    long itemId = mAdapter.getItemId(position);
-                    Integer startTop = mItemIdTopMap.get(itemId);
-                    int top = child.getTop();
-                    if (startTop != null) {
-                        if (startTop != top) {
-                            int delta = startTop - top;
-                            child.setTranslationY(delta);
-                            child.animate().setDuration(MOVE_DURATION).translationY(0);
-                            if (firstAnimation) {
-                                child.animate().withEndAction(new Runnable() {
-                                    public void run() {
-                                        mBackgroundContainer.hideBackground();
-                                        mSwiping = false;
-                                        mListView.setEnabled(true);
-                                    }
-                                });
-                                firstAnimation = false;
-                            }
-                        }
-                    } else {
-                        // Animate new views along with the others. The catch is that they did not
-                        // exist in the start state, so we must calculate their starting position
-                        // based on neighboring views.
-                        int childHeight = child.getHeight() + listview.getDividerHeight();
-                        startTop = top + (i > 0 ? childHeight : -childHeight);
-                        int delta = startTop - top;
-                        child.setTranslationY(delta);
-                        child.animate().setDuration(MOVE_DURATION).translationY(0);
-                        if (firstAnimation) {
-                            child.animate().withEndAction(new Runnable() {
-                                public void run() {
-                                    mBackgroundContainer.hideBackground();
-                                    mSwiping = false;
-                                    mListView.setEnabled(true);
-                                }
-                            });
-                            firstAnimation = false;
-                        }
-                    }
-                }
-                mItemIdTopMap.clear();
-                return true;
-            }
-        });
+    private static long getRealIdFromListView(ListView listView, int index) {
+        int firstVisiblePosition = listView.getFirstVisiblePosition();
+        return firstVisiblePosition + index;
     }
-
-
 }
