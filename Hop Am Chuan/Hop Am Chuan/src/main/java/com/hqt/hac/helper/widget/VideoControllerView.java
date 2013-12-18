032c417177c368d46f5e1e6a1c77c43e0ad5f4e1
Huynh Quang Thao <huynhquangthao@gmail.com>
2013-12-18 10:15:24 +0700

    songview

---
+++ Hop Am Chuan/Hop Am Chuan/src/main/java/com/hqt/hac/helper/widget/VideoControllerView.java
@@ -0,0 +1,7 @@
+package com.hqt.hac.helper.widget;
+
+/**
+ * Created by ThaoHQSE60963 on 12/16/13.
+ */
+public class VideoControllerView {
+}
---
+++ Hop Am Chuan/Hop Am Chuan/src/main/java/com/hqt/hac/view/test/VideoPlayerActivityTest.java
@@ -0,0 +1,7 @@
+package com.hqt.hac.view.test;
+
+/**
+ * Created by ThaoHQSE60963 on 12/16/13.
+ */
+public class VideoPlayerActivityTest {
+}
