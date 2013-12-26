432d227e9810b7d777b7833428c92bd2cb2461a8
Huynh Quang Thao <huynhquangthao@gmail.com>
2013-12-26 16:30:05 +0700

    merge

---
+++ Hop Am Chuan/Hop Am Chuan/src/main/java/com/hqt/hac/view/BunnyApplication.java
@@ -0,0 +1,25 @@
+package com.hqt.hac.view;
+
+import android.app.Application;
+
+/**
+ * Created by ThaoHQSE60963 on 12/26/13.
+ */
+public class BunnyApplication extends Application {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
+}
