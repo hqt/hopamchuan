26bbb10dfe5d277ded38b61724559353e74a0258
Huynh Quang Thao <huynhquangthao@gmail.com>
2014-01-08 03:11:51 +0700

    fix bug. not running all yet. hope work on real device

--- Hop Am Chuan/Hop Am Chuan/src/main/java/com/hqt/hac/view/MainActivity.java
+++ Hop Am Chuan/Hop Am Chuan/src/main/java/com/hqt/hac/view/MainActivity.java
@@ -353,10 +353,6 @@ public class MainActivity extends SlidingMenuActionBarActivity
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main, menu);
 
-        // because SearchView just exist in HoneyComb 3.0 to above
-        // we should check version of users here
-        // if version is lower. We use SearchDialog instead
-        // TODO: search google. Find SearchWidget library for APIUtils Lower than 11
         MenuItem searchItem = menu.findItem(R.id.search_bar);
         if (searchItem != null) {
             // Get the SearchView and set the Search Configuration
