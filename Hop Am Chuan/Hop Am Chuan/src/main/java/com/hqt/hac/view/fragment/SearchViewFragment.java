e70afba68e4d4f2b0e3c9118e542ccc36e1ad363
Huynh Quang Thao <huynhquangthao@gmail.com>
2014-01-08 21:43:59 +0700

    finish search by song. remain search by singer/author

--- Hop Am Chuan/Hop Am Chuan/src/main/java/com/hqt/hac/view/fragment/SearchViewFragment.java
+++ Hop Am Chuan/Hop Am Chuan/src/main/java/com/hqt/hac/view/fragment/SearchViewFragment.java
@@ -7,22 +7,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
-import android.widget.Spinner;
+import android.widget.*;
 import com.hqt.hac.config.Config;
+import com.hqt.hac.helper.adapter.IContextMenu;
 import com.hqt.hac.helper.adapter.SongListAdapter;
 import com.hqt.hac.helper.widget.InfinityListView;
+import com.hqt.hac.helper.widget.SongListRightMenuHandler;
 import com.hqt.hac.model.Song;
-import com.hqt.hac.model.dal.ArtistDataAccessLayer;
-import com.hqt.hac.model.dal.PlaylistDataAccessLayer;
 import com.hqt.hac.model.dal.SongDataAccessLayer;
+import com.hqt.hac.utils.DialogUtils;
 import com.hqt.hac.view.BunnyApplication;
+import com.hqt.hac.view.MainActivity;
 import com.hqt.hac.view.R;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.zip.Inflater;
 
 import static com.hqt.hac.utils.LogUtils.LOGE;
 import static com.hqt.hac.utils.LogUtils.makeLogTag;
@@ -35,8 +35,17 @@ public class SearchViewFragment extends Fragment implements IHacFragment, Infini
 
     private static String TAG = makeLogTag(SearchViewFragment.class);
 
+    /** Activity running this fragment */
+    MainActivity activity;
+
     /** List contains search data */
     InfinityListView mListView;
+
+    /** One popup menu for all items **/
+    PopupWindow popupWindow = null;
+
+    /** inflater use to inflate layout */
+    LayoutInflater inflater;
 
     /** Adapter for ListView
      * use old ones become it's same
@@ -58,6 +67,7 @@ public class SearchViewFragment extends Fragment implements IHacFragment, Infini
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
+        this.activity = (MainActivity) activity;
         // get arguments from main mActivity
         Bundle arguments = getArguments();
         if ((arguments.getString("search_key_word") != null)) {
@@ -69,6 +79,7 @@ public class SearchViewFragment extends Fragment implements IHacFragment, Infini
     @Override
     public void onDetach() {
         super.onDetach();
+        activity = null;
     }
 
     @Override
@@ -80,6 +91,7 @@ public class SearchViewFragment extends Fragment implements IHacFragment, Infini
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.activity_search_view, container, false);
+        this.inflater = inflater;
 
         mListView = (InfinityListView) rootView.findViewById(R.id.list_view);
 
@@ -99,7 +111,7 @@ public class SearchViewFragment extends Fragment implements IHacFragment, Infini
          *  this ListView is full rich function. See document for more detail
          */
         InfinityListView.ListViewProperty property = new InfinityListView.ListViewProperty();
-        property.Loader(this).Adapter(mAdapter).FirstProcessLoading(true).LoadingView(R.layout.list_item_loading)
+        property.Loader(this).FirstProcessLoading(true).LoadingView(R.layout.list_item_loading)
                 .NumPerLoading(Config.DEFAULT_SONG_NUM_PER_LOAD).RunningBackground(true);
         mListView.setListViewProperty(property);
 
@@ -114,10 +126,10 @@ public class SearchViewFragment extends Fragment implements IHacFragment, Infini
                 res = SongDataAccessLayer.searchSongByTitle(queryStr, offset, count);
                 break;
             case 1:
-                res = ArtistDataAccessLayer.searchSongBySinger(queryStr, 100);
+                //res = ArtistDataAccessLayer.searchSongBySinger(queryStr, 100);
                 break;
             case 2:
-                res = ArtistDataAccessLayer.searchSongByAuthor(queryStr, 100);
+                //res = ArtistDataAccessLayer.searchSongByAuthor(queryStr, 100);
         }
         return res;
     }
@@ -128,17 +140,31 @@ public class SearchViewFragment extends Fragment implements IHacFragment, Infini
             case 0:
                 // search by artist
                 mAdapter = new SongListAdapter(BunnyApplication.getAppContext(), new ArrayList<Song>());
+                // Event for right menu click
+                popupWindow = DialogUtils.createPopup(inflater, R.layout.popup_songlist_menu);
+                SongListRightMenuHandler.setRightMenuEvents(activity, popupWindow);
+
+                // Event received from mAdapter.
+                ((SongListAdapter)mAdapter).contextMenuDelegate = new IContextMenu() {
+                    @Override
+                    public void onMenuClick(View view, Song song, ImageView theStar) {
+                        // Show the popup menu and set selectedSong, theStar
+                        SongListRightMenuHandler.openPopupMenu(view, song, theStar);
+                    }
+                };
+                bindEventListView();
                 break;
             case 1:
                 // search by author
+                bindEventListView();
                 break;
             case 2:
                 // search by singer
+                bindEventListView();
                 break;
             default:
                 // do nothing
         }
-        // mListView.resetListView(mAdapter);
         mListView.setAdapter(mAdapter);
 
     }
@@ -151,5 +177,30 @@ public class SearchViewFragment extends Fragment implements IHacFragment, Infini
     @Override
     public int getTitle() {
         return 0;
+    }
+
+    private void bindEventListView() {
+        switch (type) {
+            case 0 : {
+                // Event for Item Click on ListView
+                mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                        SongDetailFragment fragment = new SongDetailFragment();
+                        Bundle arguments = new Bundle();
+                        arguments.putParcelable("song", ((SongListAdapter) mAdapter).songs.get(position));
+                        fragment.setArguments(arguments);
+                        activity.switchFragmentNormal(fragment);
+                        activity.changeTitleBar(((SongListAdapter)mAdapter).songs.get(position).title);
+                    }
+                });
+            }
+            case 1: {
+
+            }
+            case 2: {
+
+            }
+        }
     }
 }
